ce53974f003d167f0f2dd87b42573c866aaefa34
sAm2097 <samanugandula1518@gmail.com>
2022-09-19 09:16:18 +0200

    src

--- README.md
+++ README.md
@@ -18,7 +18,19 @@ npm run build
 ### Lints and fixes files
 ```
 npm run lint
+
 ```
+npm i express dotenv cors axios 
+
+npm i -D nodemon    
+
+npm run dev 
+
+npm install -D tailwindcss postcss autoprefixer
+
+npx tailwindcss init -p
+
+npm i axios leaflet
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,174 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-screen relative">
+    <GeoErrorModal
+      @closeGeoError="closeGeoError"
+      v-if="geoError"
+      :geoErrorMsg="geoErrorMsg"
+    />
+    <MapFeatures
+      @getGeolocation="getGeolocation"
+      @plotResult="plotResult"
+      @toggleSearchResults="toggleSearchResults"
+      @removeResult="removeResult"
+      :coords="coords"
+      :fetchCoords="fetchCoords"
+      :searchResults="searchResults"
+    />
+
+    <div id="map" class="h-full z-[1]"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import leaflet from "leaflet";
+import { onMounted, ref } from "vue";
+import GeoErrorModal from "../components/GeoErrorModal.vue";
+import MapFeatures from "../components/MapFeatures.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: { GeoErrorModal, MapFeatures },
+  setup() {
+    let map;
+    onMounted(() => {
+      //init map
+      map = leaflet.map("map").setView([28.538336, -81.379234], 10);
+
+      //add tile layer
+      leaflet
+        .tileLayer(
+          `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`,
+          {
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: process.env.VUE_APP_API_KEY,
+          }
+        )
+        .addTo(map);
+
+      map.on("moveend", () => {
+        closeSearchResults();
+      });
+      // get users location
+      getGeolocation();
+    });
+    const coords = ref(null);
+    const fetchCoords = ref(null);
+    const geoMarker = ref(null);
+    const geoError = ref(null);
+    const geoErrorMsg = ref(null);
+
+    const getGeolocation = () => {
+      if (coords.value) {
+        coords.value = null;
+        sessionStorage.removeItem("coords");
+        map.removeLayer(geoMarker.value);
+        return;
+      }
+
+      if (sessionStorage.getItem("coords")) {
+        coords.value = JSON.parse(sessionStorage.getItem("coords"));
+        plotGeolocation(coords.value);
+        return;
+      }
+      fetchCoords.value = true;
+      navigator.geolocation.getCurrentPosition(setCoords, getLocError);
+    };
+
+    const setCoords = (pos) => {
+      //stop fetching coords
+      fetchCoords.value = null;
+
+      //set coords in session storage
+      const setSessionCoords = {
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      };
+
+      sessionStorage.setItem("coords", JSON.stringify(setSessionCoords));
+
+      //set ref coords value
+      coords.value = setSessionCoords;
+
+      plotGeolocation(coords.value);
+    };
+
+    const getLocError = (err) => {
+      fetchCoords.value = null;
+      geoError.value = true;
+      geoErrorMsg.value = err.messsage;
+    };
+
+    const plotGeolocation = (coords) => {
+      //create custom marker
+      const customMarker = leaflet.icon({
+        iconUrl: require("../assets/red.png"),
+        iconSize: [35, 35],
+      });
+      //create new marker with coords and custom icon
+
+      geoMarker.value = leaflet
+        .marker([coords.lat, coords.lng], { icon: customMarker })
+        .addTo(map);
+
+      //set map view to current location
+      map.setView([coords.lat, coords.lng], 10);
+    };
+
+    const resultMarker = ref(null);
+
+    const plotResult = (coords) => {
+      // If there is already a marker, remove it. Only allow 1
+      if (resultMarker.value) {
+        map.removeLayer(resultMarker.value);
+      }
+      const customMarker = leaflet.icon({
+        iconUrl: require("../assets/blue.png"),
+        iconSize: [35, 35], // size of the icon
+      });
+      resultMarker.value = leaflet
+        .marker([coords.coordinates[1], coords.coordinates[0]], {
+          icon: customMarker,
+        })
+        .addTo(map);
+      map.setView([coords.coordinates[1], coords.coordinates[0]], 13);
+
+      closeSearchResults();
+    };
+
+    const removeResult = () => {
+      map.removeLayer(resultMarker.value);
+    };
+    const closeGeoError = () => {
+      geoErrorMsg.value = null;
+      geoError.value = null;
+    };
+
+    const searchResults = ref(null);
+    const toggleSearchResults = () => {
+      searchResults.value = !searchResults.value;
+    };
+    const closeSearchResults = () => {
+      searchResults.value = null;
+    };
+
+    return {
+      geoError,
+      closeGeoError,
+      geoErrorMsg,
+      fetchCoords,
+      coords,
+      getGeolocation,
+      plotResult,
+      searchResults,
+      toggleSearchResults,
+      closeSearchResults,
+      removeResult,
+    };
+  },
+};
 </script>
+
